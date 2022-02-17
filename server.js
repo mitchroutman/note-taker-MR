7e285d540162b0923d6726e575b15b51a006ec03
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-17 18:02:44 -0500

    added 404 html page, added html routes in server js. Route to note taking page still broken

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const { json, application } = require('express');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.port || 3001;
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) => 
+    res.send('/public/404')
+);
+
+app.listen(PORT, function() {
+    console.log('Listening on port: ' + PORT);
+});
 
