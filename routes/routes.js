1c01f0125c89459558ce7f33827edf3cd439f2af
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-18 16:14:45 -0500

    added post route

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,29 @@
 const fs = require('fs');
+const router = require('express').Router()
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 //api/notes get
-app.get('/api/notes', function(req, res) {
-    res.json(notes);
+router.get('/notes', function(req, res) {
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        const notes = JSON.parse(data)
+        res.json(notes);
+    })
 })
+
+router.post('/notes', function(req, res) {
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        const notes = JSON.parse(data);
+        let newNote = req.body;
+        newNote.id = uuidv4();
+
+        notes.push(req.body);
+
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) throw err;
+            res.json("added new info");
+        })
+    })
+})
+
+module.exports = router;
