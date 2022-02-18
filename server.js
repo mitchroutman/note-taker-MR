3248b93137242596497cdb492a5c2a18102e33fc
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-18 16:22:33 -0500

    updated middleware, reorganized

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const api = require('./routes/routes')
 
 const app = express();
 const PORT = process.env.port || 3001;
@@ -8,18 +9,22 @@ const PORT = process.env.port || 3001;
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 //HTML routes
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+//API routes
+app.use('/api', api);
 
 app.get('*', (req, res) => 
-    res.send('/public/404')
+    res.send('/public/404.html')
 );
 
 //
