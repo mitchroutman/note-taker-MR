fd157c3b629c2c4aabefda243b451c4fb7bfb20f
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-18 14:15:04 -0500

    added middleware

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.port || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
